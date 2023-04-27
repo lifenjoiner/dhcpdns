2d9af658756d99369167d35433b7e884f08d1c7b
YX Hao <lifenjoiner@163.com>
2023-04-27 18:47:27 +0800

    Add build.bat

---
+++ cli/build.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+pushd %~dp0
+
+::-code-::
+call :Build windows amd64
+call :Build linux amd64
+call :Build darwin amd64
+call :Build freebsd amd64
+::-code-::
+
+:Exit
+popd
+endlocal
+exit /b
+
+:Build
+setlocal
+set GOOS=%1
+set GOARCH=%2
+set OutDir=dhcpdns-%1-%2
+echo %OutDir%
+go build -ldflags="all=-s -w" -trimpath -o %OutDir%/
+endlocal
+exit /b
--- readme.md
+++ readme.md
@@ -4,8 +4,9 @@ It helps programs to use the upstream DNS easily when your network changed.
 ## Usage and Demo
 
 [cli](./cli) detects the DHCPv6/DHCPv4 DNS every 30s.
+Enter cli dir in CLI:
 ```
-go build cli
+go build
 cli
 ```
 
