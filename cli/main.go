7723ff0f1cee9286843213b25d0c47cbd407b3cd
YX Hao <lifenjoiner@163.com>
2023-04-27 19:06:56 +0800

    Add CI

--- cli/main.go
+++ cli/main.go
@@ -8,6 +8,7 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 	"time"
 
 	"github.com/lifenjoiner/dhcpdns"
@@ -35,6 +36,10 @@ func main() {
 		addr = "8.8.8.8:80"
 		log.Printf("Targeting: %v", addr)
 		showResult(dhcpdns.Detect4(addr))
+
+		if len(os.Args) > 1 {
+			break
+		}
 
 		time.Sleep(30 * time.Second)
 	}
